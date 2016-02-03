bf5dbb93074fade0df58ed453671bee0c6df7b20
Anthony Martin <antm88@gmail.com>
2016-02-03 02:59:41 +0000

    Corrected Login URL

--- MusicServer/AppDelegate.m
+++ MusicServer/AppDelegate.m
@@ -130,7 +130,7 @@
     NSString *token;
     NSString *authentication;
     [[self authHandler] getAuthentication:&authentication token:&token];
-    NSString *appURL = [NSString stringWithFormat:@"http://localhost:12345/#login/%@/%@", token, authentication];
+    NSString *appURL = [NSString stringWithFormat:@"http://localhost:12345/#/login/%@/%@", token, authentication];
     [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:appURL]];
 }
 
